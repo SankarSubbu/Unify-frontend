1364877a8f6f97eb3613e2709a917a1cd9bbc9be
SankarSubbu <102902715+SankarSubbu@users.noreply.github.com>
2024-02-17 17:26:46 +0530

    fixed url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
-let BASE_URL = "http://localhost:4000/";
+let BASE_URL = "https://unify-a-social-app.onrender.com/";
 
 export { BASE_URL };
